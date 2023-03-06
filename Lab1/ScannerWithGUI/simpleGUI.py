9be2a5063c744f1b4f33b56531e5bc626af5a131
120L020121 <matrix_250@163.com>
2023-03-06 11:34:53 +0800

    add config files.

--- Lab1/ScannerWithGUI/simpleGUI.py
+++ Lab1/ScannerWithGUI/simpleGUI.py
@@ -1,10 +1,11 @@
 import sys
 from PyQt6.QtWidgets import (QApplication,  QToolTip, QPushButton,
-                              QMessageBox, QMainWindow, QMenu)
+                              QMessageBox, QMainWindow, QMenu, QTextEdit, QLabel,
+                              QHBoxLayout, QVBoxLayout, QWidget)
 # from PyQt6.QtWidgets import QWidget
 from PyQt6.QtGui import QFont, QAction, QIcon
 
-class Example(QMainWindow):
+class Example(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -28,13 +29,13 @@ class Example(QMainWindow):
         btn.move(50,50)
 
         # 退出弹窗确定
-        exitAct = QAction(QIcon('exit.png'), '&Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Exit application')
-        exitAct.triggered.connect(QApplication.instance().quit)
+        # exitAct = QAction(QIcon('exit.png'), '&Exit', self)
+        # exitAct.setShortcut('Ctrl+Q')
+        # exitAct.setStatusTip('Exit application')
+        # exitAct.triggered.connect(QApplication.instance().quit)
 
         # 状态栏
-        self.statusBar()
+        # self.statusBar()
         # self.statusBar().showMessage('Ready')
 
         # 菜单栏
@@ -43,15 +44,55 @@ class Example(QMainWindow):
         # fileMenu.addAction(exitAct)
 
         # 子菜单
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('File')
-        impMenu = QMenu('Import', self)
-        impAct = QAction('Import mail', self)
-        impMenu.addAction(impAct)
-        newAct = QAction('New', self)
-        fileMenu.addAction(newAct)
-        fileMenu.addMenu(impMenu)
+        # menubar = self.menuBar()
+        # fileMenu = menubar.addMenu('File')
+        # impMenu = QMenu('Import', self)
+        # impAct = QAction('Import mail', self)
+        # impMenu.addAction(impAct)
+        # newAct = QAction('New', self)
+        # fileMenu.addAction(newAct)
+        # fileMenu.addMenu(impMenu)
         
+        # 勾选菜单栏
+        # self.statusbar = self.statusBar()
+        # self.statusbar.showMessage('Ready')
+        # menubar = self.menuBar()
+        # viewMenu = menubar.addMenu('View')
+        # viewStatAct = QAction('View statusbar', self, checkable=True)
+        # viewStatAct.setStatusTip('View statusbar')
+        # viewStatAct.triggered.connect(self.toggleMenu)
+        # viewMenu.addAction(viewStatAct)
+        
+        # 工具栏
+        # exitAct = QAction(QIcon('exit24.png'), 'Exit', self)
+        # exitAct.setShortcut('Ctrl+Q')
+        # exitAct.triggered.connect(QApplication.instance().quit)
+        # self.toolbar = self.addToolBar('Exit')
+        # self.toolbar.addAction(exitAct)
+
+        # 文本编辑窗口
+        # textEdit = QTextEdit()
+        # self.setCentralWidget(textEdit)
+
+        # 绝对布局
+        lbl1 = QLabel('ZetCode', self)
+        lbl1.move(15, 10)
+
+        # 水平、竖直布局
+        okButton = QPushButton("OK")
+        cancelButton = QPushButton("Cancel")
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
         # self.setGeometry(300, 300, 300, 200)
         self.resize(350, 250)
         self.center()
@@ -77,6 +118,23 @@ class Example(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    def toggleMenu(self, state):
+        '''勾选菜单栏'''
+        if state:
+            self.statusbar.show()
+        else:
+            self.statusbar.hide()
+
+    def contextMenuEvent(self, event):
+        '''上下文菜单'''
+        cmenu = QMenu(self)
+        newAct = cmenu.addAction("New")
+        openAct = cmenu.addAction("Open")
+        quitAct = cmenu.addAction("Quit")
+        action = cmenu.exec(self.mapToGlobal(event.pos()))
+
+        if action == quitAct:
+            QApplication.instance().quit()
 def main():
     app = QApplication(sys.argv)
     ex = Example()
